c900f81b10baaa0e916dae3f33d446d6b6a42cbc
valgaudeau <valgaudeau@outlook.com>
2023-07-08 00:49:46 +0100

    Comments for Bidir DFS ReconstructPath implementation
    Also changed AlgorithmDisplayMap speed

--- MazeMvcApp/MazeMvcApp/Models/MazeSolverAlgos/BiDirectionalDFS.cs
+++ MazeMvcApp/MazeMvcApp/Models/MazeSolverAlgos/BiDirectionalDFS.cs
@@ -6,11 +6,12 @@ namespace MazeMvcApp.Models.MazeSolverAlgos
     {
         private readonly Maze _maze;
         public List<MazeCell> ValidPath { get; set; } = new List<MazeCell>();
-        public Stack<MazeCell> PathTopStart { get; set; } = new Stack<MazeCell>();
-        public Stack<MazeCell> PathBottomStart { get; set; } = new Stack<MazeCell>();
         public Queue<MazeCell> VisitedCellsTopPath { get; set; } = new Queue<MazeCell>();
         public Queue<MazeCell> VisitedCellsBottomPath { get; set; } = new Queue<MazeCell>();
         public Dictionary<MazeCell, double> AlgorithmDisplayMap { get; set; } = new Dictionary<MazeCell, double>();
+        private Stack<MazeCell> _pathTopStart = new Stack<MazeCell>();
+        private Stack<MazeCell> _pathBottomStart = new Stack<MazeCell>();
+        private MazeCell _intersectionCell;
 
         public BiDirectionalDFS(Maze maze)
         {
@@ -22,8 +23,8 @@ namespace MazeMvcApp.Models.MazeSolverAlgos
             MazeCell startCellTopPath = _maze.StartCell;
             MazeCell startCellBottomPath = _maze.EndCell;
 
-            PathTopStart.Push(startCellTopPath);
-            PathBottomStart.Push(startCellBottomPath);
+            _pathTopStart.Push(startCellTopPath);
+            _pathBottomStart.Push(startCellBottomPath);
             VisitedCellsTopPath.Enqueue(startCellTopPath);
             VisitedCellsBottomPath.Enqueue(startCellBottomPath);
 
@@ -36,21 +37,21 @@ namespace MazeMvcApp.Models.MazeSolverAlgos
             AlgorithmDisplayMap.Add(currentCellBottomPath, delay);
             delay += 0.05;
 
-            while ( (!IsIntersecting(out MazeCell? intCell)) && (currentCellTopPath != _maze.EndCell) && (currentCellBottomPath != _maze.StartCell) )
+            while ( (!IsIntersecting(out MazeCell? intersectionCell)) && (currentCellTopPath != _maze.EndCell) && (currentCellBottomPath != _maze.StartCell) )
             {
                 if (turn == true)
                 {
                     if (IsMovePossible(currentCellTopPath, turn, out MazeCell? nextCell))
                     {
                         currentCellTopPath = nextCell;
-                        PathTopStart.Push(currentCellTopPath);
+                        _pathTopStart.Push(currentCellTopPath);
                         VisitedCellsTopPath.Enqueue(currentCellTopPath);
                         turn = false;
                     }
                     else
                     {
-                        PathTopStart.Pop();
-                        currentCellTopPath = PathTopStart.Peek();
+                        _pathTopStart.Pop();
+                        currentCellTopPath = _pathTopStart.Peek();
                     }
                 }
                 
@@ -59,14 +60,14 @@ namespace MazeMvcApp.Models.MazeSolverAlgos
                     if (IsMovePossible(currentCellBottomPath, turn, out MazeCell? nextCell))
                     {
                         currentCellBottomPath = nextCell;
-                        PathBottomStart.Push(currentCellBottomPath);
+                        _pathBottomStart.Push(currentCellBottomPath);
                         VisitedCellsBottomPath.Enqueue(currentCellBottomPath);
                         turn = true;
                     }
                     else
                     {
-                        PathBottomStart.Pop();
-                        currentCellBottomPath = PathBottomStart.Peek();
+                        _pathBottomStart.Pop();
+                        currentCellBottomPath = _pathBottomStart.Peek();
                     }
                 }
 
@@ -76,17 +77,18 @@ namespace MazeMvcApp.Models.MazeSolverAlgos
                 if (!AlgorithmDisplayMap.ContainsKey(currentCellTopPath))
                 {
                     AlgorithmDisplayMap.Add(currentCellTopPath, delay);
+                    delay += 0.05;
                 }
 
                 if (!AlgorithmDisplayMap.ContainsKey(currentCellBottomPath))
                 {
                     AlgorithmDisplayMap.Add(currentCellBottomPath, delay);
+                    delay += 0.05;
                 }
 
-                delay += 0.05;
             }
 
-            ValidPath = new List<MazeCell>(PathTopStart.Union(PathBottomStart));
+            ValidPath = new List<MazeCell>(_pathTopStart.Union(_pathBottomStart));
             ValidPath.Reverse();
             _maze.AlgorithmDisplayMap = AlgorithmDisplayMap;
             _maze.PopulateFinalDisplayTimer();
@@ -98,8 +100,8 @@ namespace MazeMvcApp.Models.MazeSolverAlgos
         {
             // Careful here that if I manipulate the stacks, it will affect the actual references used 
             // Using Paths not VisitedCells because we know there is only 1 valid path through the maze
-            MazeCell[] pathTopStartArr = PathTopStart.ToArray();
-            MazeCell[] pathBottomStartArr = PathBottomStart.ToArray();
+            MazeCell[] pathTopStartArr = _pathTopStart.ToArray();
+            MazeCell[] pathBottomStartArr = _pathBottomStart.ToArray();
             
             foreach(MazeCell topPathCell in pathTopStartArr)
             {
@@ -137,6 +139,23 @@ namespace MazeMvcApp.Models.MazeSolverAlgos
             }
             nextCell = null;
             return false;
+        }
+
+        private void ReconstructPath()
+        {
+            // if intersection cell is still null, that means either top or bottom path solved the maze,
+            // and no intersection happened between the 2 paths
+            // So I can check if top contains end else bottom contains start and return that as valid path
+
+
+
+            // if intersection cell isn't null, then I think given that this is DFS and that the maze only
+            // has 1 valid path and uses stacks, I can pop MazeCell entries from wherever the
+            // intersection cell or its neighbour (since only 1 of the stacks will contain intersection cell)
+            // is located in the stack and that's the valid path
+
+
+
         }
 
     }
